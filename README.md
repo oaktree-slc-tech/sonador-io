542a75d967537b88ee7b246c5722cc13b63f7b35
Rob Oakes <rob.oakes@oak-tree.us>
2024-11-13 14:45:45 +0000

    Added helper methods for "streaming" images as an alternative to zip archive download, added expanded README
    * Expanded README with an overview of the library, installation instructions, and links to Jupyter notebooks showing how it can be used.
    * Added `stream_imaging_series` helper task to retrieve the images for large series instance by instance to provide a fallback if download of a zip archive fails.
    * Added `--stream` option to the CLI parser.
    
    Related merge requests:
    
    * oak-tree/medical-imaging/sonador3d!8: refactor of Sonador 3D classes to enable easier deployment of dependencies (SimpleITK and VTK/PyVista) in Blender and 3D Slicer

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# Sonador Python Client
+# [Sonador IO](https://sonador.oak-tree.tech/io) Python Library
+[Sonador IO](https://sonador.oak-tree.tech/io) is the data management interface of [Sonador](https://sonador.oak-tree.tech) used to securely store medical data so that it can be queried, retrieved, and transferred. It provides a streamlined way to share medical data with across the Internet or other applications within the hospital.
+
+The [Sonador IO Python Library](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-client) provides tools for working with meta and payload (pixel) data stored in [Sonador/Orthanc](https://code.oak-tree.tech/oak-tree/medical-imaging/orthanc-sonador). It builds on top of the [Guru Client Library](https://code.oak-tree.tech/guru-labs/guru-client) and provides data models for medical imaging resources (patient, study, series), Sonador's "DICOM extension models" (which provide specialized features such as interfaces for worklists), resource comments, interfaces for working with structured data (SR documents), and interfaces for managing 3D data. _The 3D capabilities of the Sonador IO client are focused on data modelling and providing a foundation for more advanced capabilities. For tools and libraries to work with imaging volume and spatial data, refer to [Sonador 3D](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador3d)._
+
+Dependencies:
+
+* [Guru Client Library](https://code.oak-tree.tech/guru-labs/guru-client): provides [client models](https://code.oak-tree.tech/django-apps/guru/-/wikis/dev.client-tools) for interacting with Sonador/Orthanc APIs using [REST-like patterns](https://code.oak-tree.tech/django-apps/guru/-/wikis/dev.data-models).
+* [`pydicom`](https://pydicom.github.io/): tools for parsing DICOM (Digital Imagingin Medicine) data and interfacing with pixel/payload data via [`numpy`](https://numpy.org/).
+* [`highdicom`](https://github.com/ImagingDataCommons/highdicom): high-level DICOM abstractions for the Python programming language. Provides tools for parsing segmentation (`SEG`) and structured reporting (`SR`) documents so that thtey can be consumed from `numpy`.
+
+
 
 ## Installation
+Pre-built binaries of the client library are available from the [Sonador Python package](https://code.oak-tree.tech/oak-tree/medical-imaging/imaging-development-env/-/wikis/deployment.package-repository) repostiory and can be installed via `pip`:
 
 ```bash
 pip install sonador --extra-index-url https://code.oak-tree.tech/api/v4/projects/335/packages/pypi/simple
 ```
 
+To install from source, clone this repository and install the dependencies.
+
+
+
 ## Usage
-Example usage is outlined in [this notebook](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples/-/blob/master/sonador-client.image-io.ipynb).
+Examples of how to utilize the features of the client can be found in the [Sonador Examples Repository](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples). _More advanced capabilities highlighting access control, 3D capabilities, and worklists are demonstrated in the [`technical-reference` subfolder](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples/-/tree/master/technical-reference?ref_type=heads)._
+
+* Basic usage of the client is outlined in [this notebook](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples/-/blob/master/sonador-client.image-io.ipynb), and introduces three of the core classes in the library: `sonador.servers.SonadorServer`, `sonador.servers.SonadorImagingServer`, and imaging resouce models.
+* An overview of working with structured data (via DICOM-SR documents) can be found [here](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples/-/blob/master/sonador-client02.dicom-sr.ipynb?ref_type=heads).
+* Basic AI capbilities and how to interface with AI based tools (via `numpy`) are demonstrated in [this notebook](https://code.oak-tree.tech/oak-tree/medical-imaging/sonador-examples/-/blob/master/sonador-client03.ai-lung.ipynb?ref_type=heads).
--- tasks/images.py
+++ tasks/images.py
@@ -1,4 +1,5 @@
 import os, logging
+from concurrent.futures import ThreadPoolExecutor
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +16,9 @@ def download_imaging_filearchive(r, download_folder, extract=False):
 			contents should be written to disk. When True, the contents of the
 			archive will be written to the provided destination.
 	'''
+	if not os.path.exists(download_folder):
+		raise ValueError('Download folder "%s" does not exist' % download_folder)
+
 	with r.filearchive() as a:
 
 		# Extract archive contents
@@ -31,3 +35,94 @@ def download_imaging_filearchive(r, download_folder, extract=False):
 				f.write(a.raw.read())
 				logger.info('File archive for %s downloaded successfully to %s'
 					% (r.pk, os.path.join(download_folder, '%s.zip' % r.pk)))
+
+
+def stream_imaging_series(sx, download_folder, tpool=None, threads=4):
+	'''	Download the imaging data for the provided series and write contents to disk.
+		Streaming downloads make one request for each file in the series.
+
+		@input sx (sonador.imaging.orthanc.ImagingSeries): imaging series 
+			to be downloaded
+		@input download_folder (str): path to the download folder where the data should
+			be stored
+	'''
+	if not os.path.exists(download_folder):
+		raise ValueError('Download folder "%s" does not exist' % download_folder)
+
+	# Create thread pool
+	tpool = tpool or ThreadPoolExecutor(max_workers=threads)
+
+	def _download_dcm(dcm):
+		'''	Download the DCM instance and write to dest folder
+
+			@returns True if download succesful
+		'''
+		dcm_fpath = os.path.join(download_folder, '%s.dcm' % dcm.pk)
+		with open(dcm_fpath, 'wb') as f:
+			dcm.dcmfile().save_as(f)
+
+			logger.debug('DICOM file for series="%s" instance="%s" downloaded successfully: %s'
+				% (sx.pk, dcm.pk, dcm_fpath))
+
+			return True
+
+	fcount = sum(tpool.map(_download_dcm, sx.instances_collection))
+	logger.info('DICOM series="%s" downloaded successfully' % sx.pk)
+
+
+def stream_imaging_study(s, download_folder, tpool=None, threads=4, **kwargs):
+	'''	Download the imaging data for the provided study and write contents to disk.
+		Streaming downloads make one request for each file in the study's DICOM's series.
+		Series downloads delegate to `stream_imaging_series`.
+
+		@input s (sonador.imaging.orthanc.ImagingStudy): imaging study to be downloaded
+		@input download_folder (str): path to the download folder where the data should
+			be stored
+	'''
+	if not os.path.exists(download_folder):
+		raise ValueError('Download folder "%s" does not exist')
+
+	# Create thread pool
+	tpool = tpool or ThreadPoolExecutor(max_workers=threads)
+
+	logger.info('<-- Begin streaming download of study="%s" -->')
+
+	for sx in s.series_collection:
+
+		# Create subfolder for imaging series (if it does not exist)
+		sx_download_folder = os.path.join(download_folder, sx.pk)
+		if not os.path.exists(sx_download_folder):
+			os.mkdir(sx_download_folder)
+
+		stream_imaging_series(sx, sx_download_folder, tpool=tpool, **kwargs)
+
+	logger.info('<-- Streaming download of study="%s" finished successfully -->' % s.pk)
+
+
+def stream_imaging_patient(p, download_folder, tpool=None, threads=4, **kwargs):
+	'''	Download the imaging data for the provided patient and write contents to disk.
+		Streaming downloads make one request per each file in each series associated with
+		the patient. Study downloads delegate to `stream_imaging_study`.
+
+		@input p (sonador.imiaging.orthanc.ImagingPatient): imaging patient to be downloaded
+		@input download_folder (str): path to the download folder where the data
+			should be stored
+	'''
+	if not os.path.exists(download_folder):
+		raise ValueError('Download folder "%s" does not exist')
+
+	# Create thread pool
+	tpool = tpool or ThreadPoolExecutor(max_workers=threads)
+
+	logger.info('<--## Begin streaming download of patient="%s" ##-->')
+
+	for s in p.studies_collection:
+
+		# Create subfolder for imaging study (if it does not exist)
+		s_download_folder = os.path.join(download_folder, s.pk)
+		if not os.path.exists(s_download_folder):
+			os.mkdir(s_download_folder)
+
+		stream_imaging_study(s, s_download_folder, tpool=tpool, **kwargs)
+
+	logger.info('<--## Streaming download of patient="%s" finished successfully ##-->' % p.pk)
